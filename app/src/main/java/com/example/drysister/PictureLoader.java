db34a39c393bf7240e759a2b7c200848bde92c70
Your Name <you@example.com>
2020-06-06 10:26:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/drysister/PictureLoader.java
@@ -0,0 +1,65 @@
+package com.example.drysister;
+
+import android.widget.ImageView;
+
+import java.util.logging.Handler;
+
+public class PictureLoader {
+    private ImageView loadImg;
+    private String imgUrl;
+    private byte[] picByte;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Handler msg) {
+            super.handleMessage(msg);
+            if (msg.what == 0x123) {
+                if (picByte != null) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(picByte, 0, picByte.length);
+                    loadImg.setImageBitmap(bitmap);
+                }
+            }
+        }
+    };
+
+    public void load(ImageView loadImg, String imgUrl) {
+        this.loadImg = loadImg;
+        this.imgUrl = imgUrl;
+        Drawable drawable = loadImg.getDrawable();
+        if(drawable != null && drawable instanceof BitmapDrawable) {
+            Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+            if(bitmap != null && !bitmap.isRecycled()) {
+                bitmap.recycle();
+            }
+        }
+        new Thread(runnable).start();
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(imgUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setReadTimeout(10000);
+                if (conn.getResponseCode() == 200) {
+                    InputStream in = conn.getInputStream();
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    byte[] bytes = new byte[1024];
+                    int length = -1;
+                    while ((length = in.read(bytes)) != -1) {
+                        out.write(bytes, 0, length);
+                    }
+                    picByte = out.toByteArray();
+                    in.close();
+                    out.close();
+                    handler.sendEmptyMessage(0x123);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+}
